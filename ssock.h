959bc72c3277d2ed7020bcacc95b4ba6451d9ac7
Sébastien Serre <sserre.bx@gmail.com>
2017-02-28 12:01:13 +0100

    initial

---
+++ ssock.h
@@ -0,0 +1,117 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/poll.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <errno.h>
+#include <string.h>
+
+#define CLASS(p) ((*(unsigned char*)(p))>>6)
+#define TCP 1
+#define UDP 0
+
+static int
+parseip(char *name, uint32_t *ip) {
+    unsigned char addr[4];
+    char *p;
+    int i, x;
+
+    p = name;
+    for (i = 0; i < 4 && *p; i++) {
+        x = strtoul(p, &p, 0);
+        if (x < 0 || x >= 256)
+            return -1;
+        if (*p != '.' && *p != 0)
+            return -1;
+        if (*p == '.')
+            p++;
+        addr[i] = x;
+    }
+
+    switch (CLASS(addr)) {
+        case 0:
+        case 1:
+            if (i == 3) {
+                addr[3] = addr[2];
+                addr[2] = addr[1];
+                addr[1] = 0;
+            } else if (i == 2) {
+                addr[3] = addr[1];
+                addr[2] = 0;
+                addr[1] = 0;
+            } else if (i != 4)
+                return -1;
+            break;
+        case 2:
+            if (i == 3) {
+                addr[3] = addr[2];
+                addr[2] = 0;
+            } else if (i != 4)
+                return -1;
+            break;
+    }
+    *ip = *(uint32_t*) addr;
+    return 0;
+}
+
+int
+netlookup(char *name, uint32_t *ip) {
+    struct hostent *he;
+
+    if (parseip(name, ip) >= 0)
+        return 0;
+
+    if ((he = gethostbyname(name)) != 0) {
+        *ip = *(uint32_t*) he->h_addr;
+        return 0;
+    }
+
+    return -1;
+}
+
+int
+netdial(int istcp, char *server, int port) {
+    int proto, fd, n;
+    uint32_t ip;
+    struct sockaddr_in sa;
+    socklen_t sn;
+
+    if (netlookup(server, &ip) < 0)
+        return -1;
+
+    proto = istcp ? SOCK_STREAM : SOCK_DGRAM;
+    if ((fd = socket(AF_INET, proto, 0)) < 0) {
+        return -1;
+    }
+
+    /* for udp */
+    if (!istcp) {
+        n = 1;
+        setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &n, sizeof n);
+    }
+
+    /* start connecting */
+    memset(&sa, 0, sizeof sa);
+    memmove(&sa.sin_addr, &ip, 4);
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(port);
+    if (connect(fd, (struct sockaddr*) &sa, sizeof sa) < 0 && errno != EINPROGRESS) {
+        close(fd);
+        return -1;
+    }
+
+    sn = sizeof sa;
+    if (getpeername(fd, (struct sockaddr*) &sa, &sn) >= 0) {
+        return fd;
+    }
+
+    /* report error */
+    sn = sizeof n;
+    getsockopt(fd, SOL_SOCKET, SO_ERROR, (void*) &n, &sn);
+    if (n == 0)
+        n = ECONNREFUSED;
+    close(fd);
+    errno = n;
+    return -1;
+}
---
+++ ssock_test.c
@@ -0,0 +1,41 @@
+/*
+ * File:   ssock_test.c
+ * Author: Sébastien Serre <sserre at msha.fr>
+ *
+ * Created on 27 février 2017, 16:58
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ssock.h>
+
+char HTTP_GET[] =
+        "GET / HTTP/1.0\r\n"
+        "Host: www.example.com\r\n\r\n";
+
+#define PAYLOAD_MAX 100000
+
+/*
+ *
+ */
+int
+main(int argc, char** argv) {
+    int fd, count;
+    char payload[PAYLOAD_MAX + 1];
+
+    if ((fd = netdial(TCP, "www.example.com", 80)) < 0) {
+        return (EXIT_FAILURE);
+    }
+
+
+    if (write(fd, HTTP_GET, strlen(HTTP_GET)) >= 0) {
+        while ((count = read(fd, payload, PAYLOAD_MAX)) > 0) {
+            printf("%s", payload);
+        }
+    }
+
+    return (EXIT_SUCCESS);
+}
+
